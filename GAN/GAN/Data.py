73077efe92fddbef7e71606b59b5e64fc33131c0
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2019-03-29 16:27:55 +0700

    Commenting codes.

--- GAN/GAN/Data.py
+++ GAN/GAN/Data.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+# Dữ liệu (x, y) được tạo thông qua hàm f(x) = 10 + x^2. Như vậy, đồ thị sẽ có dạng parabol. 
 def get_y(x):
     return 10 + x*x
 
-
+# Khởi tạo dữ liệu ngẫu nhiên. 
 def sample_data(n=10000, scale=100):
     data = []
 
+    # Tạo mảng số ngẫu nhiên gồm 10000 phần tử trong giới hạn từ 0-0.5 -> 1-0.5
     x = scale*(np.random.random_sample((n,))-0.5)
 
     for i in range(n):
@@ -15,5 +17,6 @@ def sample_data(n=10000, scale=100):
 
     return np.array(data)
 
+# Hàm tạo noise ngẫu nhiên. 
 def sample_Z(m, n):
     return np.random.uniform(-1., 1., size=[m, n])
--- GAN/GAN/GAN.py
+++ GAN/GAN/GAN.py
@@ -12,12 +12,15 @@ G_sample = gen.generator(Z)
 r_logits, r_rep = disc.discriminator(X)
 f_logits, g_rep = disc.discriminator(G_sample,reuse=True)
 
+# Định nghĩa hàm loss. 
 disc_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=r_logits,labels=tf.ones_like(r_logits)) + tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.zeros_like(f_logits)))
 gen_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.ones_like(f_logits)))
 
+# Các tham chiếu sử dụng trong suốt quá trình session chạy được gọi là variable. 
 gen_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Generator")
 disc_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Discriminator")
 
+# Định nghĩa optimizer là RMSProp, hiệu quả hơn so với back propagation truyền thống khi dùng với vanilla GAN. 
 gen_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(gen_loss,var_list = gen_vars) # G Train step
 disc_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(disc_loss,var_list = disc_vars) # D Train step
 
@@ -30,25 +33,40 @@ ng_steps = 10
 x_plot = Data.sample_data(n=batch_size)
 
 for i in range(10001):
+    # Lấy dữ liệu X có n là kích thước batch. 
     X_batch = Data.sample_data(n=batch_size)
+
+    # Tạo noise. 
     Z_batch = Data.sample_Z(batch_size, 2)
+
+    # Huấn luyện discriminator. 
     for _ in range(nd_steps):
         _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
+
+    # Chú thích: 
+    # rrep_dstep: Dữ liệu thật (output của h3) trước khi generate tại thời điểm i. 
+    # grep_dstep: Dữ liệu giả (output của h3) trước khi generate tại thời điểm i. 
     rrep_dstep, grep_dstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
 
+    # Huấn luyện generator. 
     for _ in range(ng_steps):
         _, gloss = sess.run([gen_step, gen_loss], feed_dict={Z: Z_batch})
 
+    # Chú thích: 
+    # rrep_gstep: Dữ liệu thật (output của h3) sau khi generate tại thời điểm i. 
+    # grep_gstep: Dữ liệu giả (output của h3) sau khi generate tại thời điểm i. 
     rrep_gstep, grep_gstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
     if (i % 100 == 0):
         print ("Iterations: {} Discriminator loss: {:.2f} Generator loss: {:.2f}".format(i,dloss,gloss))
         
         plt.figure()
+
+        # Chạy gen để ra output dữ liệu giả. 
         g_plot = sess.run(G_sample, feed_dict={Z: Z_batch})
         xax = plt.scatter(x_plot[:,0], x_plot[:,1])
         gax = plt.scatter(g_plot[:,0],g_plot[:,1])
 
-        plt.legend((xax,gax), ("Real Data","Generated Data"))
+        plt.legend((xax,gax), ("Dữ liệu thật","Dữ liệu giả"))
         plt.title('Samples at Iteration %d'%i)
         plt.tight_layout()
         plt.savefig('plots/iterations/iteration_%d.png'%i)
@@ -61,24 +79,9 @@ for i in range(10001):
         grg = plt.scatter(grep_gstep[:,0], grep_gstep[:,1], alpha=0.5)
 
 
-        plt.legend((rrd, rrg, grd, grg), ("Real Data Before G step","Real Data After G step",
-                               "Generated Data Before G step","Generated Data After G step"))
-        plt.title('Transformed Features at Iteration %d'%i)
+        plt.legend((rrd, rrg, grd, grg), ("Dữ liệu thật trước khi gen","Dữ liệu thật sau khi gen",
+                               "Dữ liệu giả trước khi gen","Dữ liệu giả sau khi gen"))
+        plt.title('Transformed Features at i = %d'%i)
         plt.tight_layout()
         plt.savefig('plots/features/feature_transform_%d.png'%i)
-        plt.close()
-
-        plt.figure()
-
-        rrdc = plt.scatter(np.mean(rrep_dstep[:,0]), np.mean(rrep_dstep[:,1]),s=100, alpha=0.5)
-        rrgc = plt.scatter(np.mean(rrep_gstep[:,0]), np.mean(rrep_gstep[:,1]),s=100, alpha=0.5)
-        grdc = plt.scatter(np.mean(grep_dstep[:,0]), np.mean(grep_dstep[:,1]),s=100, alpha=0.5)
-        grgc = plt.scatter(np.mean(grep_gstep[:,0]), np.mean(grep_gstep[:,1]),s=100, alpha=0.5)
-
-        plt.legend((rrdc, rrgc, grdc, grgc), ("Real Data Before G step","Real Data After G step",
-                               "Generated Data Before G step","Generated Data After G step"))
-
-        plt.title('Centroid of Transformed Features at Iteration %d'%i)
-        plt.tight_layout()
-        plt.savefig('plots/features/feature_transform_centroid_%d.png'%i)
         plt.close()
